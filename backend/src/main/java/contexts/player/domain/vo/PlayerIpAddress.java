9b44eeb0ee23980abdd509d2e116936d2690fbbf
rbernabeulopez <rafael.bernabeu@bosonit.com>
2023-01-20 09:54:47 +0100

    Fixed value objects access

--- backend/src/main/java/contexts/player/domain/vo/PlayerIpAddress.java
+++ backend/src/main/java/contexts/player/domain/vo/PlayerIpAddress.java
@@ -3,6 +3,7 @@ package contexts.player.domain.vo;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import contexts.player.domain.exception.InvalidValueException;
 
@@ -12,9 +13,10 @@ import java.net.UnknownHostException;
 @Getter
 @EqualsAndHashCode
 @ToString
+@NoArgsConstructor
 public class PlayerIpAddress {
     @JsonValue
-    private final String ipAddress;
+    private String ipAddress;
 
 
     private boolean isValidIp(String ip) {
