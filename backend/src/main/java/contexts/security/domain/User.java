7ea0f8ce51ccd6d448e2affebeb288cad096a3bc
rbernabeulopez <rafael.bernabeu@bosonit.com>
2023-01-23 11:00:52 +0100

    Configured Spring Security

---
+++ backend/src/main/java/contexts/security/domain/User.java
@@ -0,0 +1,53 @@
+package contexts.security.domain;
+
+import contexts.player.domain.entities.Player;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+public class User implements UserDetails {
+    private Player player;
+
+    public User(String username) {
+        player = Player.builder().name(username).build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.player.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.player.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ backend/src/main/java/contexts/security/infrastructure/SecurityConfig.java
@@ -0,0 +1,59 @@
+package contexts.security.infrastructure;
+
+import contexts.security.application.UserFinder;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private UserFinder userAppService;
+    private final JwtConfig jwtConfig;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .userDetailsService(this.userAppService)
+                .addFilterAfter(jwtTokenVerifierFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll().and()
+                .authenticationManager(authenticationManager(http));
+
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+
+        return authenticationManagerBuilder.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+
+    @Bean
+    public JwtTokenVerifierFilter jwtTokenVerifierFilter() {
+        return new JwtTokenVerifierFilter(this.jwtConfig.getKey());
+    }
+}
