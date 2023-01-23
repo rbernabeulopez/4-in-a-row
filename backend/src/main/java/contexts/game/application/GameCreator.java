906935fb7381d36ba59cace408803e4ec59316c4
Pablo RDev <pabloriojod@gmail.com>
2023-01-23 10:28:26 +0100

    Updated from develop: playerFinder

--- backend/src/main/java/contexts/game/application/GameCreator.java
+++ backend/src/main/java/contexts/game/application/GameCreator.java
@@ -2,28 +2,27 @@ package contexts.game.application;
 
 import contexts.game.domain.entity.Game;
 import contexts.game.domain.repository.GameRepository;
+import contexts.player.application.PlayerFinder;
 import contexts.player.domain.entities.Player;
-import contexts.player.domain.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class GameCreator {
     @Autowired
     private GameRepository gameRepository;
     @Autowired
-    private PlayerRepository playerRepository; // TODO: change to PlayerFinder
+    private PlayerFinder playerFinder;
 
-    public Game createGame(
-            Long player1Id
-    ) {
-        Optional<Player> player = playerRepository.findById(player1Id);
+    public Game createGame(Long player1Id) {
+        Player player = playerFinder.findPlayer(player1Id);
 
-        return gameRepository.save(Game.builder()
-                .players(List.of(player.get()))
-                .build());
+        return gameRepository.save(
+                Game.builder()
+                .players(List.of(player))
+                .build()
+        );
     }
 }
