ab6e8298ffaef717084464badf0e43a2aca4f5c9
Pablo RDev <pabloriojod@gmail.com>
2023-01-23 10:35:09 +0100

    Updated from develop in GamePostControllerTest

--- backend/src/test/java/contexts/game/infrastructure/GamePostControllerTest.java
+++ backend/src/test/java/contexts/game/infrastructure/GamePostControllerTest.java
@@ -1,18 +1,15 @@
 package contexts.game.infrastructure;
 
-import contexts.IntegrationTestCase;
+import contexts.IntegrationTestsBase;
 import contexts.player.domain.entities.Player;
 import contexts.player.domain.repository.PlayerRepository;
-import contexts.player.domain.vo.PlayerIpAddress;
-import contexts.player.domain.vo.PlayerName;
-import contexts.player.domain.vo.PlayerPassword;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
-class GamePostControllerTest extends IntegrationTestCase {
+class GamePostControllerTest extends IntegrationTestsBase {
     @Autowired
     private PlayerRepository playerRepository;
     private HttpServletRequest httpServletRequest;
@@ -22,9 +19,9 @@ class GamePostControllerTest extends IntegrationTestCase {
     void createGameOK() throws Exception {
         GamePostRequest gamePostRequest = GamePostRequestMother.basic();
         Player player = playerRepository.save(Player.builder()
-                .name(new PlayerName("test"))
-                .password(new PlayerPassword("testtest"))
-                .ipAddress(new PlayerIpAddress("0:0:0:0:0:0:0:1"))
+                .name("test")
+                .password("testtest")
+                .ipAddress("0:0:0:0:0:0:0:1")
                 .build());
 
         MvcResult mvcResult = assertRequestWithBody(
