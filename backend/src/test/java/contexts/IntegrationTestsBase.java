238b2bfc10ed85f0ff89e147e9772048d6ce4787
Pablo RDev <pabloriojod@gmail.com>
2023-01-25 10:24:59 +0100

    FIX test SQL error

--- backend/src/test/java/contexts/IntegrationTestsBase.java
+++ backend/src/test/java/contexts/IntegrationTestsBase.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -34,8 +33,8 @@ public class IntegrationTestsBase {
 
     @AfterEach
     void tearDown() {
-        gameRepository.deleteAll();
         playerRepository.deleteAll();
+        gameRepository.deleteAll();
     }
 
 
