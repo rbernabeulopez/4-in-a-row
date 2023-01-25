73f8f7a290c416dd45f3d17531a8ef7a8b0f1b9d
Alex Ccachuco <robertoalexander.ccachuco@bosonit.com>
2023-01-25 11:44:41 +0100

    add file css

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './table.css'
 
 export const Table =() => {
 
@@ -118,7 +119,6 @@ export const Table =() => {
 
     return (
         <div>
-          
           <table>
             <thead>
             </thead>
