7b8fc7d0fb916e20460dc1592a6ac4150c6470d6
rbernabeulopez <rafael.bernabeu@bosonit.com>
2023-01-25 09:36:44 +0100

    Added default and not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
 import { Register } from "./view/Register";
 import Login from "./view/Login";
+import NotFound from "./view/NotFound";
+import {useEffect} from "react";
 
+
+const Redirect = () => {
+    const navigate = useNavigate();
+    useEffect(() =>
+        navigate(localStorage.getItem('playerToken') ? '/create-game' : '/login'),
+    []);
+}
 
 const App = () => {
   return (
       <BrowserRouter>
           <Routes>
+              <Route path="/" element={<Redirect />} />
               <Route path="/register">
                   <Route index element={<Register/>}/>
               </Route>
               <Route path="/login">
                   <Route index element={<Login />}/>
               </Route>
+              <Route path="*" element={<NotFound/>}/>
           </Routes>
       </BrowserRouter>
   );
