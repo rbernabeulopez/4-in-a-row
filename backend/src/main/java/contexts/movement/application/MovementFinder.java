af6b9a7008e2e0ac43515bc648d9f7a67c804461
Pablo Fuentes <pablo.fuentes@bosonit.com>
2023-01-27 11:12:35 +0100

    fix promesa definitivo

--- backend/src/main/java/contexts/movement/application/MovementFinder.java
+++ backend/src/main/java/contexts/movement/application/MovementFinder.java
@@ -1,6 +1,4 @@
 package contexts.movement.application;
-import contexts.game.domain.entity.Game;
-import contexts.game.domain.repository.GameRepository;
 import contexts.movement.domain.entity.Movement;
 import contexts.movement.domain.repository.MovementRepository;
 import lombok.AllArgsConstructor;
@@ -13,14 +11,11 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 public class MovementFinder {
-
-    private GameRepository gameRepository;
     private MovementRepository movementRepository;
 
-    public List<Movement> findMovementsById(long id) {
-        log.info("Searching movements for game with id {}", id);
-        Game game = gameRepository.findById(id).orElseThrow();
-        return movementRepository.getByGameId(game);
+    public List<Movement> findMovementsById(long gameId) {
+        log.info("Searching movements for game with id {}", gameId);
+        return movementRepository.getByGameId(gameId);
     }
 
 }
