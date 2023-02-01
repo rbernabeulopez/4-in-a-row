1f628665aad19a60dc0774ce81327d894d319d47
rbernabeulopez <rafael.bernabeu@bosonit.com>
2023-02-01 10:56:05 +0100

    Removed debug button

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -139,7 +139,6 @@ export const Table =() => {
                     </table>
                     </Col>
                     </Row>
-          <button onClick={() => console.log(data)}>PULSAME</button>
         </div>
     )
 }
